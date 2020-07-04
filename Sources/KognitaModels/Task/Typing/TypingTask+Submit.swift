d531779c0dc146c05066fb6752bf98bf61a2c3ca
Mats Eikeland Mollestad <mem@mollestad.no>
2020-07-04 18:15:28 +0200

    [TypingTask] Added public init

--- Sources/KognitaModels/Task/Typing/TypingTask+Submit.swift
+++ Sources/KognitaModels/Task/Typing/TypingTask+Submit.swift
@@ -2,6 +2,13 @@ import Foundation
 
 extension TypingTask {
     public struct Submit: Codable {
+        public init(timeUsed: TimeInterval?, knowledge: Double, taskIndex: Int, answer: String) {
+            self.timeUsed = timeUsed
+            self.knowledge = knowledge
+            self.taskIndex = taskIndex
+            self.answer = answer
+        }
+
         public let timeUsed: TimeInterval?
         public let knowledge: Double
         public let taskIndex: Int
