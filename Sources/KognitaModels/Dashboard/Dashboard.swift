5b3144791c715acd6931b53dae5d05d6ebcca4f8
Mats Eikeland Mollestad <mem@mollestad.no>
2020-10-24 12:41:59 +0200

    [RecommendedRecap] Added subjectID and recommendedRecap to Dashbard

--- Sources/KognitaModels/Dashboard/Dashboard.swift
+++ Sources/KognitaModels/Dashboard/Dashboard.swift
@@ -1,16 +1,18 @@
 public struct Dashboard: Codable {
 
-    public init(subjects: [Subject.ListOverview], ongoingPracticeSession: PracticeSession.ID?, ongoingTestSession: TestSession.ID?, openedTest: SubjectTest.UserOverview?) {
+    public init(subjects: [Subject.ListOverview], ongoingPracticeSession: PracticeSession.ID?, ongoingTestSession: TestSession.ID?, openedTest: SubjectTest.UserOverview?, recommendedRecap: RecommendedRecap?) {
         self.subjects = subjects
         self.ongoingPracticeSession = ongoingPracticeSession
         self.ongoingTestSession = ongoingTestSession
         self.openedTest = openedTest
+        self.recommendedRecap = recommendedRecap
     }
 
     public let subjects: [Subject.ListOverview]
     public let ongoingPracticeSession: PracticeSession.ID?
     public let ongoingTestSession: TestSession.ID?
     public let openedTest: SubjectTest.UserOverview?
+    public let recommendedRecap: RecommendedRecap?
 
     public var activeSubjects: [Subject.ListOverview] { subjects.filter({ $0.isActive }) }
     public var inactiveSubjects: [Subject.ListOverview] { subjects.filter({ $0.isActive == false }) }
